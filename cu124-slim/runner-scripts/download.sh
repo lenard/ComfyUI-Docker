86fa383b2f390f3f94147641e2d1e9df31d48d71
lenard <lenardandal@gmail.com>
2025-02-28 15:55:40 -0500

    Update download.sh
    preexisting /root/ComfyUI dir when trying to setup for windows with Docker mounted dirs for models, custom_notes, input and output

--- cu124-slim/runner-scripts/download.sh
+++ cu124-slim/runner-scripts/download.sh
@@ -24,7 +24,9 @@ echo "########################################"
 
 set +e
 cd /root
-git clone https://github.com/comfyanonymous/ComfyUI.git || git -C "ComfyUI" pull --ff-only
+git clone https://github.com/comfyanonymous/ComfyUI.git temp
+# ComfyUI dir already exists... so we want to try and turn it into a git repo
+mv ./temp/.git /root/ComfyUI/.git
 cd /root/ComfyUI
 # Using stable version (has a release tag)
 git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
